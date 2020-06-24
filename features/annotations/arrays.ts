4c1f765e121b2f820c1eb95aef9ba75b9365ddf6
Pratikkshirsagar <meetpratiik@gmail.com>
2020-06-24 19:23:50 +0530

    arrays and tuple added

---
+++ features/annotations/arrays.ts
@@ -0,0 +1,5 @@
+const carMakers: string[] = ['ford', 'toyota', 'chevy'];
+
+const carsByMake: string[][] = [['f150'], ['corolla'], ['camaro']];
+
+const importantDates: (Date | string)[] = [new Date(), '2020-12-12'];
---
+++ features/annotations/tuples.ts
@@ -0,0 +1,10 @@
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  suger: 40,
+};
+
+type Drink = [string, boolean, number];
+
+const pepsi: Drink = ['brown', true, 40];
+const sprite: Drink = ['clear', false, 50];
